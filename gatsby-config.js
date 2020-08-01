3e0d346e4562cb2e382107e3fb1d17d9cbd27633
Weslley Araujo <weslleyaraujo20@gmail.com>
2020-08-01 16:44:44 +0200

    feat(posts): Migrate all posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,80 +1,87 @@
-require(`dotenv`).config({
-  path: `.env`,
-})
+require("dotenv").config({
+  path: ".env",
+});
 
-const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE;
 
 module.exports = {
   siteMetadata: {
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
+    siteTitle: "Weslley Araujo",
+    siteTitleAlt: "Weslley Araujo",
+    siteUrl: "https://weslleyaraujo.dev",
+    author: "@_weslleyaraujo",
+    siteLanguage: "en",
   },
   plugins: [
     {
-      resolve: `@lekoarts/gatsby-theme-minimal-blog`,
+      resolve: "@lekoarts/gatsby-theme-minimal-blog",
       // See the theme's README for all available options
       options: {
         navigation: [
           {
-            title: `Blog`,
-            slug: `/blog`,
+            title: "Blog",
+            slug: "/blog",
           },
           {
-            title: `About`,
-            slug: `/about`,
+            title: "About",
+            slug: "/about",
           },
         ],
         externalLinks: [
           {
-            name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
+            name: "Twitter",
+            url: "https://twitter.com/_weslleyaraujo",
           },
           {
-            name: `Instagram`,
-            url: `https://www.instagram.com/lekoarts.de/`,
+            name: "Linkedin",
+            url: "https://www.linkedin.com/in/araujoweslley",
+          },
+          {
+            name: "Github",
+            url: "https://github.com/weslleyaraujo",
           },
         ],
       },
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: "gatsby-plugin-google-analytics",
       options: {
         trackingId: process.env.GOOGLE_ANALYTICS_ID,
       },
     },
-    `gatsby-plugin-sitemap`,
+    "gatsby-plugin-sitemap",
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: "gatsby-plugin-manifest",
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
-        start_url: `/`,
-        background_color: `#fff`,
-        theme_color: `#6B46C1`,
-        display: `standalone`,
+        name: "Weslley Araujo",
+        short_name: "Weslley Araujo",
+        start_url: "/",
+        background_color: "#fff",
+        theme_color: "#6B46C1",
+        display: "standalone",
         icons: [
           {
-            src: `/android-chrome-192x192.png`,
-            sizes: `192x192`,
-            type: `image/png`,
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-            src: `/android-chrome-512x512.png`,
-            sizes: `512x512`,
-            type: `image/png`,
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
           },
         ],
       },
     },
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-netlify`,
+    "gatsby-plugin-offline",
+    "gatsby-plugin-netlify",
     shouldAnalyseBundle && {
-      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
+      resolve: "gatsby-plugin-webpack-bundle-analyser-v2",
       options: {
-        analyzerMode: `static`,
-        reportFilename: `_bundle.html`,
+        analyzerMode: "static",
+        reportFilename: "_bundle.html",
         openAnalyzer: false,
       },
     },
   ].filter(Boolean),
-}
+};
